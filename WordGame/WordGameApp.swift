23630296b2098c6de6559634ef130ecd038ef446
Nishthajain7 <jnishtha305@gmail.com>
2025-08-21 09:33:33 +0530

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by Nishtha Jain on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
